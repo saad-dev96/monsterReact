1661d82df9fad1d64eeeb8711eada2636e312bb9
saadgitt <saad.akbar@tintash.com>
2020-07-24 14:28:05 +0500

    searchField: search field added but not active

---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component }  from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { CardList } from './component/card-list/card-list.component'
+
+class App extends Component 
+{
+  constructor()
+  {
+    super();
+    this.state= { monsters : [], searchField: [] };
+  }
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=> this.setState({monsters:users}))
+  }
+ 
+  render() {
+    return (
+      <div className="App">
+        <input type='search' placeholder='Search Monsters' onChange={ e=> { this.setState({searchField: e.target.value}, () => (console.log(this.state)))}}/>
+        <CardList monsters = {this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
